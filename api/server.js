658f5b43fc61b8db7142d8acc3043a359ad481e9
Ethan Edmond <eedmond@uccs.edu>
2021-07-05 15:35:54 -0600

    Finished mvp, all tests succeeding

--- api/server.js
+++ api/server.js
@@ -2,16 +2,31 @@
 const express = require('express');
 const Users = require('./users/model');
 
-console.log(Users);
-
 const server = express();
 server.use(express.json());
 
+const catch500 = (res) => {
+  return (err) => {
+    res.status(500).json({ message: err.message });
+  };
+};
+
 // POST /api/users
 server.post('/api/users', (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ message: "Posted to /api/users" });
+  if (req.body.name && req.body.bio) {
+    const { name, bio } = req.body;
+    Users.insert({ name, bio })
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'There was an error while saving the user to the database' });
+      });
+  } else {
+    res.status(400).json({ message: 'Please provide name and bio for the user' });
+  }
 });
+
 // GET /api/users
 server.get('/api/users', (req, res) => {
   Users.find()
@@ -19,23 +34,61 @@ server.get('/api/users', (req, res) => {
       res.status(200).json(users);
     })
     .catch(err => {
-      res.status(500).json({message: err.message});
+      res.status(500).json({ message: 'The users information could not be retrieved' });
     });
 });
+
 // GET /api/users/:id
 server.get('/api/users/:id', (req, res) => {
   const id = req.params.id;
-  res.status(200).json({message: `Got /api/users/${id}`});
+  Users.findById(id)
+    .then(user => {
+      if(user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'The user information could not be retrieved' });
+    });
 });
+
 // DELETE /api/users/:id
 server.delete('/api/users/:id', (req, res) => {
   const id = req.params.id;
-  res.status(200).json({message: `Deleted /api/users/${id}`});
+  Users.remove(id)
+    .then(user => {
+      if(user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'The user could not be removed' });
+    });
 });
+
 // PUT /api/users/:id
-server.get('/api/users', (req, res) => {
-  const id = req.params.id;
-  res.status(200).json({message: `Put /api/users/${id}`});
+server.put('/api/users/:id', (req, res) => {
+  if (req.body.name && req.body.bio) {
+    const { name, bio } = req.body;
+    const id = req.params.id;
+    Users.update(id, { name, bio })
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+	} else {
+	  res.status(404).json({ message: 'The user with the specified ID does not exist' });
+	}
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'The user information could not be modified' });
+      });
+  } else {
+    res.status(400).json({ message: 'Please provide name and bio for the user' });
+  }
 });
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
