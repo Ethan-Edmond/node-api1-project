6ae776ce9eed251e4f1403f1bd1e456fa3ead9fe
Ethan Edmond <eedmond@uccs.edu>
2021-07-05 14:25:45 -0600

    made skeleton for server requests and finished get /api/users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,41 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const Users = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+console.log(Users);
+
+const server = express();
+server.use(express.json());
+
+// POST /api/users
+server.post('/api/users', (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ message: "Posted to /api/users" });
+});
+// GET /api/users
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message});
+    });
+});
+// GET /api/users/:id
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({message: `Got /api/users/${id}`});
+});
+// DELETE /api/users/:id
+server.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({message: `Deleted /api/users/${id}`});
+});
+// PUT /api/users/:id
+server.get('/api/users', (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({message: `Put /api/users/${id}`});
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
